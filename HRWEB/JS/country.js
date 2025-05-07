baa02e4dc87cf405ddb1b3b86d295e9b4ddf90dd
Anchalbai-12 <bscs2312145@szabist.pk>
2025-05-07 13:17:46 +0000

    Index.Html and Script

---
+++ HRWEB/JS/country.js
@@ -0,0 +1,24 @@
+const Link = "https://literate-train-5g5jvgpqv546c9jj-5005.app.github.dev/country";
+
+fetch(Link).then(response=>{
+    if(!response.ok){
+        throw new Error("Failed to fetch data");
+    }
+    return response.json();
+}).then(data=>{
+    const tbody = document.querySelector("#countrytable tbody");
+
+    data.forEach(c=>{
+        const row = document.createElement("tr");
+
+        row.innerHTML=`
+        <td>${c.country_id}</td>
+        <td>${c.country_name}</td>
+        <td>${c.region_id}</td>   
+    `;
+    tbody.appendChild(row);
+});
+
+}).catch(err=>{
+    console.log(err.message);
+});
---
+++ HRWEB/JS/employee.js
@@ -0,0 +1,32 @@
+const Link = "https://literate-train-5g5jvgpqv546c9jj-5005.app.github.dev/employee";
+
+fetch(Link).then(response=>{
+    if(!response.ok){
+        throw new Error("Failed to fetch data");
+    }
+    return response.json();
+}).then(data=>{
+    const tbody = document.querySelector("#employeetable tbody");
+
+    data.forEach(e=>{
+        const row = document.createElement("tr");
+
+        row.innerHTML=`
+        <td>${e.employee_id}</td>
+        <td>${e.first_name}</td>
+        <td>${e.last_name}</td>   
+        <td>${e.email}</td>  
+        <td>${e.phone_number}</td>  
+        <td>${e.hire_date}</td>  
+        <td>${e.job_id}</td>  
+        <td>${e.salary}</td>  
+        <td>${e.commission_pct}</td> 
+        <td>${e.manager_id}</td> 
+        <td>${e.department_id}</td> 
+    `;
+    tbody.appendChild(row);
+});
+
+}).catch(err=>{
+    console.log(err.message);
+});
